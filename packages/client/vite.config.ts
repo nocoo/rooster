1d39b4ff139fb607cd8a4580b60a382c7fa52be2
Zheng Li <lizheng@lizheng.me>
2026-05-26 12:33:17 +0800

    chore: allow rooster.dev.hexly.ai as Vite dev host

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [preact()],
   server: {
     port: 7037,
+    allowedHosts: ['rooster.dev.hexly.ai'],
     proxy: {
       '/api': 'http://localhost:7038',
       '/socket.io': {
